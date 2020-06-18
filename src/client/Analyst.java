5c5c044d30e6f6784ec576c6a23a1daee363f36f
AlexTsiouris <alextsiouris95@gmail.com>
2020-06-18 20:42:10 +0300

    First Commit

---
+++ src/client/Analyst.java
@@ -0,0 +1,8 @@
+package client;
+
+
+public class Analyst {
+
+    System.out.println("If you want to EXIT the program at any given moment type Finished and press ENTER");
+    System.out.println("Please insert the path to the file you want to load")
+}
